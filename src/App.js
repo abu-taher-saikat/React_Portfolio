d32ce676c814ff213e18774e1659975af518abcc
Nabil Woalid Rafiu <nabilwoalidrafiu@gmail.com>
2021-05-22 00:50:52 +0600

    service added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import TopBar from './Components/NavBar';
 import Hero from './Sections/Hero';
-
+import Service from './Sections/Service';
 
 function App() {
   return (
     <div className="App">
         <TopBar />
         <Hero></Hero>
+        <Service/>
     </div>
   );
 }
