60fe914268dbcb1179ac5fedb826b028c3319742
saikat1095 <mdabuthaersaikat@gmail.com>
2021-05-22 00:42:54 +0600

    slider added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
-import TopBar from './Components/NavBar';
-import Hero from './Sections/Hero';
+// import TopBar from './Components/NavBar';
+// import Hero from './Sections/Hero';
+// import Work from './Sections/Work';
+import Gallery from './Components/Gallery';
+
 
 
 function App() {
   return (
     <div className="App">
-        <TopBar />
-        <Hero></Hero>
+        {/* <TopBar /> */}
+        {/* <Hero></Hero> */}
+        {/* <Work></Work> */}
+        <Gallery></Gallery>
     </div>
   );
 }
--- src/Sections/Work.js
+++ src/Sections/Work.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from 'react';
+import { Swiper, SwiperSlide } from "swiper/react";
+import {Container} from 'react-bootstrap';
+
+// Import Swiper styles
+import "swiper/swiper.min.css";
+import "swiper/components/effect-coverflow/effect-coverflow.min.css";
+import "swiper/components/pagination/pagination.min.css";
+
+// Import Images
+import img1 from '../Assets/images/p1-1.jpg'
+import img2 from '../Assets/images/p2-2.jpg'
+import img3 from '../Assets/images/p3-3.jpg'
+
+// import Swiper core and required modules
+import SwiperCore, { EffectCoverflow, Pagination } from "swiper/core";
+
+
+// install Swiper modules
+SwiperCore.use([EffectCoverflow, Pagination]);
 
 const Work = () => {
     return (
-        <div>
-            
+        <div className="work">
+        <Container>
+            <Swiper
+                effect={"coverflow"}
+                grabCursor={true}
+                centeredSlides={true}
+                slidesPerView={"auto"}
+                coverflowEffect={{
+                    rotate: 50,
+                    stretch: 0,
+                    depth: 100,
+                    modifier: 1,
+                    slideShadows: true
+                }}
+                pagination={true}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                <img className="work_img"  alt="work images" src={img1} />
+                </SwiperSlide>
+                <SwiperSlide>
+                <img className="work_img"  alt="work images" src={img2} />
+                </SwiperSlide>
+                <SwiperSlide>
+                <img  className="work_img" alt="work images" src={img3} />
+                </SwiperSlide>
+            </Swiper>
+        </Container>
         </div>
     )
 }
