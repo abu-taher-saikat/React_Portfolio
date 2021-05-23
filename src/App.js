d248c26592afdf28838ea3159e5ce9289706c17c
saikat1095 <mdabuthaersaikat@gmail.com>
2021-05-24 00:18:45 +0600

    done

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import './App.css';
 import TopBar from './Components/NavBar';
 import Blog from './Sections/Blog/Blog.js';
 import Contact from './Sections/Contact';
-<<<<<<< HEAD
-
-=======
->>>>>>> 63c087dc43e511fd58ed0a5e9e91da2ca36b0f27
 import Hero from './Sections/Hero';
 import Service from './Sections/Service';
 import Work from './Sections/Work';
