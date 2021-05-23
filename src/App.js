fd83b75db3be2263f46251d9affcb1346d624fa4
NAHID MURAD ABIR <nahid.muradabir@gmail.com>
2021-05-24 00:09:16 +0600

    paralex section done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Experience from "./Components/Experience";
 import TopBar from "./Components/NavBar";
 import Blog from "./Sections/Blog/Blog.js";
 import Hero from "./Sections/Hero";
+import HireMe from "./Sections/HireMe";
 import Reviews from "./Sections/Reviews";
 import Service from "./Sections/Service";
 
@@ -15,6 +16,7 @@ function App() {
       <Blog />
       <Reviews />
       <Experience />
+      <HireMe />
     </div>
   );
 }
