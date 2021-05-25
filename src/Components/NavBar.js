821b411b480e2397164a199f5dfe58da21d9003c
saikat1095 <mdabuthaersaikat@gmail.com>
2021-05-26 01:07:26 +0600

    doen

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -2,23 +2,28 @@ import React,{useState, useEffect} from 'react';
 import { Nav, Navbar } from 'react-bootstrap';
 
 const TopBar = () => {
-    const [offset, setOffset] = useState(0);
+    const [opacity, setOpacity] = useState(0);
 
-    const scrollFunction  = () => {
-
+    const scrollFunction  = (e) => {
+        console.log(e.target.value)
     }
 
     useEffect(() => {
         window.onscroll = () => {
-          setOffset(window.pageYOffset)
+        let currentScrollPos1 = window.pageYOffset;
+        if(currentScrollPos1 > 600){
+            setOpacity(1);
+        }else{
+            setOpacity(0)
         }
-        console.log(offset)
-      }, [offset]);
+        }
+        // console.log(offset)
+      }, [setOpacity]);
 
 
 
     return (
-        <div style={offset > 800 ? {display : "block"} : {display : "none"}} className="TopBar" onScroll={scrollFunction}>
+        <div style={{opacity : `${opacity}`}} className="TopBar" onScroll={scrollFunction}>
             <div className="container">
                 <Navbar  expand="lg" >
                 <Navbar.Brand href="#home">Portfolio</Navbar.Brand>
